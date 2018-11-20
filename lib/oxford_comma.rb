83c305f375f7b20076edb7bc937b77dc0ea52bb4
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-20 02:43:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  
+  if array.size < 2
+    return array.join
+  else
+    array.insert(-2, 'and')
+    if array.size > 3
+      return array.join(',')
+    else
+      return array.join
+    end
+  end
 end
